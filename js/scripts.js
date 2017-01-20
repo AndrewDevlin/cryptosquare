2f2d584c2923f993b3f293ac3977bddb2d351a70
andrew devlin and philip putnam <andrew@>
2017-01-19 16:43:10 -0800

    add columns back-end function in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
+
+var boxRows = function(array) {
+  var dimensions = array.length;
+  var dimensions = Math.ceil(Math.sqrt(dimensions));
+
+  if ((dimensions * (dimensions - 1)) >= array.length) {
+    dimensions = dimensions - 1;
+  }
+
+  return dimensions;
+}
+
 $(document).ready(function(){
   $("#crypto-form").submit(function(event){
     event.preventDefault();
 
     var toCode = $("input#user-input").val();
 
-
     toCode = toCode.match(/[a-z]|[1-9]/gi);
+
+    var columns = boxRows(toCode);
 
     $(".output").text(toCode);
 
+    alert(columns);
 
 
   });
